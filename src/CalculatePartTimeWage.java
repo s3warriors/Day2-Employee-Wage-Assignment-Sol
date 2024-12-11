9e6475c267dd1f3785a5790de9ff7e7120396ae6
s3warriors <satveerswami982@gmail.com>
2024-12-11 16:17:11 +0530

    Added: Calculate Part Time Wage Feature

---
+++ src/CalculatePartTimeWage.java
@@ -0,0 +1,10 @@
+public class CalculatePartTimeWage {
+    private static final int WAGE_PER_HOUR = 20;
+    private static final int PART_TIME_HOUR = 4;
+
+    public static void main(String[] args) {
+        System.out.println("UC3: Calculate Part-Time Wage");
+        int partTimeWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+        System.out.println("Part-Time Wage: " + partTimeWage);
+    }
+}
