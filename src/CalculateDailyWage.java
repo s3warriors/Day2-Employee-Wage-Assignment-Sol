1e24c322214c2b58b9ec38f37b4cad478f6811b5
s3warriors <satveerswami982@gmail.com>
2024-12-11 16:16:04 +0530

    Added: Calculate Daily Wage Feature

---
+++ src/CalculateDailyWage.java
@@ -0,0 +1,10 @@
+public class CalculateDailyWage {
+    private static final int WAGE_PER_HOUR = 20;
+    private static final int FULL_DAY_HOUR = 8;
+
+    public static void main(String[] args) {
+        System.out.println("UC2: Calculate Daily Wage");
+        int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        System.out.println("Full-Time Daily Wage: " + dailyWage);
+    }
+}
