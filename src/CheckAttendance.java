e6371f35a74172dfb635020e3d71b8ba4fb4277c
s3warriors <satveerswami982@gmail.com>
2024-12-11 16:14:58 +0530

    Added: Check Attendance Feature

---
+++ src/CheckAttendance.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class CheckAttendance {
+    public static void main(String[] args) {
+        System.out.println("UC1: Check Employee Attendance");
+        Random random = new Random();
+        int isPresent = random.nextInt(2); // 0 = Absent, 1 = Present
+
+        if (isPresent == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+    }
+}
+
